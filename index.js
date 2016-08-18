2d9f5e324f5909317b395a3469bd339b448c9c01
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2016-08-18 16:26:10 -0600

    Add rudimentary parser.  Still much needed to be fully functional.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const fs = require('fs')
 const createLexer = require('./lexer')
+const parse = require('./parser')
 
 const s = fs.readFileSync('./sample.d.ts', 'utf-8')
 const lexer = createLexer(s)
-let t
-while ((t = lexer.next()).type != '$EOF')
-	console.log(t.type, [ 'ID', 'REFERENCE_COMMENT', 'STRING' ].includes(t.type) ? t.match : '')
+const ast = parse(lexer)
+console.log(require('util').inspect(ast, null, null))
+
